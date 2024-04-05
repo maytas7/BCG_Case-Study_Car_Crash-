0016d4efe5e121c2cf290d5a69f21b7b3fc921e5
Satyam Singh <78936192+maytas7@users.noreply.github.com>
2024-04-06 00:47:20 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ __author__ = "satyam.singh"
 from setuptools import find_packages, setup
 
 setup(
-    name="bcg",
+    name="car_crash_case_study",
     version="0.0.1",
     packages=find_packages(),
     zip_safe=False,
